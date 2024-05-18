40f47f4a3cb9207cf56f9f8a0a41d2baf746202b
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-05-18 11:24:02 -0500

    Update addItem function in useOrder hook
    The `addItem` function in `useOrder.ts` hook was updated to handle cases where an item is already in the order. If an item is already present, it increments the quantity by one instead of adding a duplicate item. Removed unnecessary console log.

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -6,11 +6,24 @@ export const useOrder = () => {
     const [order, setOrder] = useState<OrderItem[]>([])
 
     const addItem = (item: ItemMenu) => {
-        const newItem: OrderItem = {...item, quantity: 1}
-        setOrder([...order, newItem])
-    }
 
-    console.log(order)
+        const itemExist = order.find(orderItem => orderItem.id === item.id)
+        if (itemExist) {
+
+            const updatedOrder = order.map((item: OrderItem) => item.id === item.id ? {
+                ...item,
+                quantity: item.quantity + 1
+            } : item)
+            setOrder(updatedOrder)
+
+
+        } else {
+            const newItem: OrderItem = {...item, quantity: 1}
+            setOrder([...order, newItem])
+        }
+
+
+    }
 
 
     return {
